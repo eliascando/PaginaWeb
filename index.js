5c282c5773e7eb0da22ef0f8485a0871a742f1e1
Elias Esteban Cando Jaime <elias.candoj@ug.edu.ec>
2023-04-11 16:19:28 -0500

    Actualización Mayor

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-var visitas = 0;
+var visitas = parseInt(localStorage.getItem('visitas')) || 0;
 
 function confirmarSalida() {
-	if (confirm('¿Esta seguro de que desea salir de la pagina actual?')) {					window.open('https://campusvirtual2.ug.edu.ec/login/index.php', '_blank');
-	}
+	if (confirm('¿Esta seguro de que desea salir de la pagina actual?')) {window.open('https://youtu.be/dQw4w9WgXcQ', '_blank');}
 }
 
 function actualizarContador() {
   visitas++;
-  document.getElementById('contador').innerHTML = visitas;
+  document.getElementById('contador-visitas').innerHTML = visitas;
+  localStorage.setItem('visitas', visitas.toString());
 }
+
+function actualizarReloj() {
+  var reloj = document.getElementById('reloj');
+  var fecha = new Date();
+  var horas = fecha.getHours();
+  var minutos = fecha.getMinutes();
+  var segundos = fecha.getSeconds();
+  horas = (horas < 10) ? '0' + horas : horas;
+  minutos = (minutos < 10) ? '0' + minutos : minutos;
+  segundos = (segundos < 10) ? '0' + segundos : segundos;
+  var horaActual = horas + ':' + minutos + ':' + segundos;
+  reloj.innerHTML = horaActual;
+}
+setInterval(actualizarReloj, 1000);
